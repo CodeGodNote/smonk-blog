7e057ecf47ffaa027baf20a74473acdab24336a7
CodeGod <75322363@qq.com>
2023-02-23 23:51:50 +0800

    文章测试

---
+++ assets/mermaid-mindmap.esm.min-6732a62d.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-97dbbe5d.js";import"./framework-4ca3085c.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-268cad43.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
